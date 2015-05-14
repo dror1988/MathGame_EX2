478a3ffb2eacc9cbb9ef99ebfd627676ff993bf1
mayaBu <mayabugana@gmail.com>
2015-05-14 07:07:24 +0300

    +

--- shotsArray.h
+++ shotsArray.h
@@ -1,3 +1,25 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// shotArray.h
+// -----------
+// This file declares a class to manage the creation, addition to and deletion of shots array
+//
+// Author: Maya Bugana
+// First version: 2015-05-10
+// 
+// This code is part of a solution for "the math game" excercise in C++ course, Semester B 2015
+// at the Academic College of Tel-Aviv-Yaffo.
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Changes and additions:
+// ------------------------
+// DATE           Authors                 Change / Addition
+// ----           --------                -----------------
+// 2015-04-10    Maya Bugana     implemented the shotArray class
+// 2015-04-13	 Dror Mpyal      Adding functions
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+// "NEW CODE EX2, author=Maya Bugana, date=2015-04-10
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+
 #ifndef _SHOTS_ARRAY_H
 #define _SHOTS_ARRAY_H
 
@@ -9,24 +31,39 @@ class shotsArray{
 	unsigned int size;
 	Shot* Arr[30];
 public:
+	// shotsArray constructor
 	shotsArray();
+	// shotsArray cdestructor
 	~shotsArray();
+	// shotsArray copy constructor
 	shotsArray(const shotsArray& a) : size(a.size)
 	{
 		*Arr = new Shot;
 		**Arr = **(a.Arr);
 	}
-
-	void initArr();
-	void printShots() const;
+	
+	// size getter
 	unsigned int getSize();
+	// Returns a shot in array index
 	Shot* getShotInArr(int i);
+	// size setter
 	unsigned int setSize(unsigned int newSize);
+	// inits the array 
+	void initArr();
+	// print the shots array
+	void printShots() const;
+	// control the movement of all shots in array
 	void move();
+	// add shot to the array
 	int addShot(Point shotPosition, Direction::VALUE shotDirection);
+	// delete shot in index i
 	void delShot(int i);
+	// swap between 2 shots
 	void swap(Shot* &a, Shot* &b);
 };
 
 #endif
 
+//+++++++++++++++++++++
+// END of NEW CODE EX2
+//+++++++++++++++++++++
